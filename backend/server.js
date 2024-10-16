df026293a0562e90fe27ed6cbb2133afdbc1ed65
Subhani Stark <smartboysubani2@gmail.com>
2024-10-16 22:02:02 +0530

    Installed Express and tested the server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app=express();
+dotenv.config();
+
+app.listen(8080, () => {
+    console.log(`app is listening on port 8080`)
+})
